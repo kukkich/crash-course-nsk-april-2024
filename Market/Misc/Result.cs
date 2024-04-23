fd993dff1cf5ccf8efb39745e292e7ba3090ce3c
kukkich <vitia.iezhovqqqq@gmail.com>
2024-04-23 22:55:25 +0700

    Добавил тип результата

---
+++ Market/Misc/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace Market.Misc;
+
+public readonly struct Result<TValue, TError>
+{
+    public bool IsFailure { get; }
+    public bool IsSucceed => !IsFailure;
+
+    private readonly TValue? _value;
+    private readonly TError? _error;
+    
+    public Result(TValue value)
+    {
+        _value = value;
+        _error = default;
+
+        IsFailure = false;
+    }
+
+    public Result(TError error)
+    {
+        _error = error;
+        _value = default;
+
+        IsFailure = true;
+    }
+
+    public static implicit operator Result<TValue, TError>(TValue value) => new(value);
+
+    public static implicit operator Result<TValue, TError>(TError error) => new(error);
+
+    public T Match<T>(Func<TValue, T> onSuccess, Func<TError, T> onFailure) 
+        => IsFailure ? onFailure(_error!) : onSuccess(_value!);
+}
